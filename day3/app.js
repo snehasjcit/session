9ad170e2e18a171a7384a68b40da0fe0e046da1e
sneha <you@example.com>
2019-10-26 13:52:33 -0700

    example

---
+++ day3/app.js
@@ -0,0 +1,27 @@
+    function myfunction1 (elem) {
+       console.log ("alert")
+       alert("this is an alert");
+    }
+
+    function myfunction2 (elem) {
+        console.log  ("alert")
+        alert("this is double alert") 
+    }
+        
+     function myfunction3 (elem) {
+        console.log ("alert")
+        alert("this is  mouse alert") 
+    }
+            
+     function sayHi () {
+       // debugger
+        console.log(event.target.value);
+   }
+            
+    function sayHELLO(event,e) {
+        alert("alert")
+     }
+     function newfun()
+     {
+         document.getElementById('changed').innerText=event.target.value
+     }
